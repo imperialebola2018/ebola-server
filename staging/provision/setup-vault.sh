d1546dff697f7bf6964c3af6025ad2e681bca3fb
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-05-25 09:52:26 +0100

    Install more recent vault

--- staging/provision/setup-vault.sh
+++ staging/provision/setup-vault.sh
@@ -4,13 +4,14 @@ set -ex
 if which -a vault > /dev/null; then
     echo "vault is already installed"
 else
-	echo "installing vault"
-	sudo apt-get update
-	sudo apt-get install -y unzip
-	vault_zip=vault_0.7.3_linux_amd64.zip
-	wget https://releases.hashicorp.com/vault/0.7.3/$vault_zip
-	unzip $vault_zip
-	chmod 755 vault
-	sudo cp vault /usr/bin/vault
-	rm -f $vault_zip vault
+    echo "installing vault"
+    sudo apt-get update
+    sudo apt-get install -y unzip
+    VAULT_VERSION=0.10.1
+    VAULT_ZIP=vault_${VAULT_VERSION}_linux_amd64.zip
+    wget https://releases.hashicorp.com/vault/${VAULT_VERSION}/${VAULT_ZIP}
+    unzip $VAULT_ZIP
+    chmod 755 vault
+    sudo cp vault /usr/bin/vault
+    rm -f $VAULT_ZIP vault
 fi
