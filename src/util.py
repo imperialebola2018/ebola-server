8a4c6c34438a80018880682fc01b98991c9f5d14
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-06-05 09:16:42 +0100

    Export to tar

--- src/util.py
+++ src/util.py
@@ -1,4 +1,5 @@
 import os.path
+import re
 
 import docker
 
@@ -41,6 +42,16 @@ def directory_to_volume(src, dest, client, verbose, remove=False, update=False):
     print("Done")
 
 
+def volume_export(src, dest, client, verbose):
+    # This is possibly a bit magic, not sure:
+    re_tar = re.compile(r"\.tar(\.(gz|bz2))?$")
+    m = re_tar.search(dest)
+    if m:
+        volume_to_archive(src, dest, m.groups()[1], client, verbose)
+    else:
+        volume_to_directory(src, dest, client, verbose)
+
+
 def volume_to_directory(src, dest, client, verbose):
     if not volume_exists(src, client):
         raise Exception("Volume '{}' does not exist".format(src))
@@ -59,7 +70,36 @@ def volume_to_directory(src, dest, client, verbose):
     src_vol = docker.types.Mount(target="/src", source=src, read_only=True)
     dest_bind = docker.types.Mount(target="/dest", source=os.path.abspath(dest),
                                   type="bind")
-    print("Exporting data from volume to disk")
+    print("Exporting data from volume to directory on disk")
+    res = client.containers.run(image=RSYNC_IMAGE, command=cmd,
+                                remove=True, mounts=[src_vol, dest_bind])
+    if verbose:
+        print(res.decode("UTF-8"))
+    print("Done")
+
+
+def volume_to_archive(src, dest, compression, client, verbose):
+    if not volume_exists(src, client):
+        raise Exception("Volume '{}' does not exist".format(src))
+
+    if compression:
+        target = "/dest.tar.{}".format(compression)
+        flags = "-{}cvf".format({"gz": "z", "bz2": "j"}[compression])
+    else:
+        target = "/dest.tar"
+        flags = "-cvf"
+
+    cmd = ["tar", flags, target, "/src/"]
+
+    # Create the file so that we get reasonable permissions:
+    with open(dest, "wb") as f:
+        pass
+
+    src_vol = docker.types.Mount(target="/src", source=src, read_only=True)
+    dest_bind = docker.types.Mount(target=target,
+                                   source=os.path.abspath(dest),
+                                   type="bind")
+    print("Exporting data from volume to tar archive")
     res = client.containers.run(image=RSYNC_IMAGE, command=cmd,
                                 remove=True, mounts=[src_vol, dest_bind])
     if verbose:
